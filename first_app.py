a6092d3b1089309506693d2b5a66c44428ce34cc
jeewisin <jeewisin@users.noreply.github.com>
2023-09-16 18:12:30 +0800

    third commit of render-app-small change to Hello world msg

--- first_app.py
+++ first_app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'This is the first flask app running on Render Web Service!'
+    return 'This is the first flask app running on Render Web Service! live update!'
 
 if __name__ == '__main__':
     app.run()
